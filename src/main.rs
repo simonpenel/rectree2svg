4482b23605e7d05435dd20ee6e9ad0270b2b5726
Simon Penel <simon.penel@univ-lyon1.fr>
2021-01-18 14:34:09 +0100

    Utilistation de la structure NoeudSVG

--- src/main.rs
+++ src/main.rs
@@ -4,50 +4,71 @@ use taxonomy::Taxonomy;
 use tree2svg::*;
 
 fn main() {
-    // Test l'Arena
-    let mut tree: ArenaTree<String> = ArenaTree::default();
-    let hello = tree.node("Hello".into());
-    println!("Index de Hello = {}",hello);
-    let world = tree.node("World".into());
-    println!("Index de World = {}",world);
-    let coucou = tree.node(String::from("Coucou"));
-    println!("Index de Coucou = {}",coucou);
-    let coucou = tree.node(String::from("World"));
-    println!("Index de World = {}",coucou);
-        let coucou = tree.new_node(String::from("World"));
-        println!("Index de World = {:?}",coucou);
-
-    println!("ARENA :{:?}",tree);
-
-    tree.arena[hello].children.push(world);
-    tree.arena[world].parent = Some(hello);
-
-    println!("ARENA :{:?}",tree);
-
-    // let mut tree_svg: ArenaTree<NoeudSVG> = ArenaTree::default();
-    // let mut noeud_svg_1 = NoeudSVG {
-    //         identifiant: 0,
-    //         name : String::from("Racine"),
-    //         x: 12.1,
-    //         y: 23.5,
-    //         e: Event::Undef,
-    //     };
-    // let  racine_svg: NoeudSVG = Default::default();
-    // let  racine_svg = NoeudSVG {
-    //         identifiant: 1,
-    //         name : String::from("Racine"),
-    //         x: 12.1,
-    //         y: 23.5,
-    //         e: Event::Speciation,
-    //     };
+    // // Test l'Arena
+    // let mut tree: ArenaTree<String> = ArenaTree::default();
+    // let hello = tree.node("Hello".into());
+    // println!("Index de Hello = {}",hello);
+    // let world = tree.node("World".into());
+    // println!("Index de World = {}",world);
+    // let coucou = tree.node(String::from("Coucou"));
+    // println!("Index de Coucou = {}",coucou);
+    // let coucou = tree.node(String::from("World"));
+    // println!("Index de World = {}",coucou);
+    // let coucou = tree.new_node(String::from("World"));
+    // println!("Index de World = {:?}",coucou);
+    // let coucou = tree.node(String::from("World"));
+    // println!("Index de Coucou = {}",coucou);
     //
-    // let racine = tree_svg.node(racine_svg);
+    // println!("Noeud associé l'index {} = {:?}",coucou ,tree.arena[coucou]);
+    // println!("ARENA :{:?}",tree);
     //
-    // let  feuille_svg: NoeudSVG = Default::default();
-    // let feuille = tree_svg.node(feuille_svg);
+    // tree.arena[hello].children.push(world);
+    // tree.arena[world].parent = Some(hello);
     //
-    // tree_svg.arena[racine].children.push(feuille);
-    // tree_svg.arena[feuille].parent = Some(racine);
-    //
-    // println!("TREE SVG:{:?}",tree_svg);
+    // println!("ARENA :{:?}",tree);
+
+    let mut tree_svg: ArenaTree<NoeudSVG> = ArenaTree::default();
+    let mut noeud1 = NoeudSVG {
+            identifiant: 0,
+            name : String::from("Racine"),
+            x: 12.1,
+            y: 23.5,
+            e: Event::Undef,
+        };
+    let  racine: NoeudSVG = Default::default();
+    let  feuille: NoeudSVG = Default::default();
+    let mut tree_svg: ArenaTree<NoeudSVG> = ArenaTree::default();
+    let racine_idx = tree_svg.node(racine);
+    let feuille_idx = tree_svg.node(feuille);
+
+    tree_svg.arena[racine_idx].children.push(feuille_idx);
+    tree_svg.arena[feuille_idx].parent = Some(racine_idx);
+
+    println!("TREE SVG:{:?}",tree_svg);
+
+    let  racine = NoeudSVG {
+            identifiant: 0,
+            name : String::from("Racine"),
+            x: 0.0,
+            y: 0.0,
+            e: Event::Undef,
+        };
+    let  feuille = NoeudSVG {
+                identifiant: 0,
+                name : String::from("Feuille"),
+                x: 0.0,
+                y: 0.0,
+                e: Event::Undef,
+            };
+
+    let mut tree_svg: ArenaTree<NoeudSVG> = ArenaTree::default();
+    let racine_idx = tree_svg.node(racine);
+    let feuille_idx = tree_svg.node(feuille);
+
+    tree_svg.arena[racine_idx].children.push(feuille_idx);
+    tree_svg.arena[feuille_idx].parent = Some(racine_idx);
+
+    println!("TREE SVG:{:?}",tree_svg);
+
+
 }
