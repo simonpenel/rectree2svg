0c0b0c10cc9a6067d05a97cc6320cfacab8db2b4
Simon Penel <simon.penel@univ-lyon1.fr>
2021-01-18 11:02:56 +0100

    Supprime  le code obsolete

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,4 @@
 use taxonomy::Taxonomy;
-/// Un noeud présente  un nom,  un identifiant et une liste de descendants qui peut être vide.
-/// La liste de descendants est une référence car on  veut garder la propriété (borower)
-/// Elle a le même temps de vie (lifetime) que le noeud père, ainsi que les noeuds de la liste.
-/// Elle est mutable car on peut ajouter des descendants.
-// #[derive(Debug)]
-// pub struct Noeud <'a> {
-//     name: String,
-//     id: usize,
-//     children: &'a mut Vec<Noeud<'a>>,
-// }
-//
-// impl <'a> Noeud <'a>{
-//     pub fn new(name: String, children: &'a mut Vec<Noeud<'a>>) -> Noeud<'a> {
-//          Noeud {
-//              name: name,
-//              id: 0,
-//              children: children,
-//          }
-//     }
-//     pub fn add(&mut self, noeud: Noeud<'a>) -> Result<(),  &'static str> {
-//         self.children.push(noeud);
-//         Ok(())
-//     }
-//     pub fn get_children(& self) -> &Vec<Noeud<'a>> {
-//         self.children
-//     }
-// }
 
 // Taken from https://dev.to/deciduously/no-more-tears-no-more-knots-arena-allocated-trees-in-rust-44k6
 #[derive(Debug)]
@@ -113,27 +86,3 @@ impl NoeudSVG {
 
 
 }
-
-// pub fn explore_arbre_ref(  n: & Noeud) {
-//     println!("Explore le noeud {:?}",n);
-//     let children = n.get_children();
-//     let v1_iter = children.iter();
-//     for (i, c) in v1_iter.enumerate() {
-//         println!("->Fils numero {}: {:?}",i, c);
-//         explore_arbre_ref(& c);
-//     }
-// }
-//
-// // Fonction liée à la structure GeneralTaxonomy de taxonomy
-// pub fn deroule_taxo(t: &taxonomy::GeneralTaxonomy, n: usize) {
-//     let children = &t.children(n).expect("Pas de fils");
-//     let name = t.from_internal_id(n).expect("Pas de nom");
-//     let parent = t.parent(n);
-//     println!("Deroule Taxonomy : parent de {:?} =  {:?}",n,parent);
-//     println!("Deroule Taconomy : fils de {:?} ({}) =  {:?}",n,name,children);
-//
-//     for child in children {
-//         println!("fils  {:?}",child);
-//         deroule_taxo(& t,*child);
-//         }
-// }
--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,6 @@ use taxonomy::Taxonomy;
 use tree2svg::*;
 
 fn main() {
-    // Cree le noeud Ma Racine
-    // let mut v: Vec<Noeud> = Vec::new();
-    // let mut  noeud = Noeud::new(String::from("Ma Racine"),&mut v);
-
-    // Cree le noeud "Feuille 1"
-    // let mut v1: Vec<Noeud> = Vec::new();
-    // let feuille1 = Noeud::new(String::from("Feuille 1"),&mut v1);
-
-    // Cree le noeud "Feuille 2"
-    // let mut v2: Vec<Noeud> = Vec::new();
-    // let feuille2 = Noeud::new(String::from("Feuille 2"),&mut v2);
-
-    //  Ajoute "Feuille 1" et "Feuille 2" à "Ma Racine"
-    // noeud.add(feuille1).expect("Impossible d'ajouter le noeud");
-    // noeud.add(feuille2).expect("Impossible d'ajouter le noeud");
-
-    // Cree le noeud "Ancetre" parent de "Ma Racine"
-    // let mut v3: Vec<Noeud> = Vec::new();
-    // v3.push(noeud);
-    // let mut  noeud2 = Noeud::new(String::from("Ancetre"),&mut v3);
-
-    // Cree le noeud "Bof"
-    // let mut v4: Vec<Noeud> = Vec::new();
-    // let noeud3 = Noeud::new(String::from("Bof"),&mut v4);
-
-    //  Ajoute "Bof" et "Feuille 2" à "Ancetre"
-    // noeud2.add(noeud3).expect("Impossible d'ajouter le noeud");
-
-    //  Explore l'arbre
-    // explore_arbre_ref(& noeud2);
-
-
     // Test l'Arena
     let mut tree: ArenaTree<String> = ArenaTree::default();
     let hello = tree.node("Hello".into());
@@ -68,20 +36,5 @@ fn main() {
 
     noeud_svg_1.check();
     noeud_svg_1.check();
-    // Teste le module de lecture de Taxonomie
-    // let mut f = File::open("newick.txt").expect("Impossible d'ouvrir le fichier");
-    // let phyl = newick::load_newick(&mut f);
-    // let phyl = match phyl {
-        // Ok(taxo) => {
-            // println!("Le fichier est correct");
-            // taxo},
-        // Err(error) => {
-                // panic!("Probleme lors de la lecture du fichier : {:?}", error);
-            // }
-    // };
-    // println!("Arbre : {:?}",phyl);
-    // let racine: &str = phyl.root();
-    // let racine_tid = phyl.to_internal_id(racine).expect("Pas de racine");
-    // Explore la taxonomie
-     // deroule_taxo(& phyl,racine_tid);
+
 }
