dbf13b1dda1703d1a4f3c3667149e4255ccdc88b
Simon Penel <simon.penel@univ-lyon1.fr>
2021-04-01 14:44:02 +0200

    Code cleaning: no more long lines

--- src/drawing.rs
+++ src/drawing.rs
@@ -27,8 +27,9 @@ pub fn draw_tree (tree: &mut ArenaTree<String>, name: String, options: &Options)
     let smallest_y = tree.get_smallest_y() * 1.0 + 0.0 ;
     let width_svg = largest_x - smallest_x + 0.0;
     let width_svg = width_svg * 1.0;
-    let height_svg = largest_y - smallest_y + 2.0 * BLOCK; // Crade ; ajout car les fuielles de l'espece sont
-                                                           // allongées lors de la creation du le path
+    let height_svg = largest_y - smallest_y + 2.0 * BLOCK;  // Ajout car les fuielles de l'espece
+                                                            // sont allongées lors de la creation
+                                                            // du  path . A ameliorer
     let height_svg = height_svg * 1.0;
     let x_viewbox = smallest_x - 0.0 ;
     let y_viewbox = smallest_y - 0.0;
@@ -56,7 +57,8 @@ pub fn draw_tree (tree: &mut ArenaTree<String>, name: String, options: &Options)
             Event::Duplication =>  g.append(get_carre(index.x,index.y,2.0,"blue".to_string())),
             Event::Loss =>  {
                 let mut cross = get_cross(index.x,index.y,2.0,"blue".to_string());
-                cross.assign("transform","rotate(45 ".to_owned()+&index.x.to_string()+" "+&index.y.to_string()+")");
+                cross.assign("transform","rotate(45 ".to_owned()+&index.x.to_string()
+                + " "+&index.y.to_string()+")");
                 g.append(cross);
             },
             _   =>  g.append(get_circle(index.x,index.y,2.0,"blue".to_string())),
@@ -67,7 +69,8 @@ pub fn draw_tree (tree: &mut ArenaTree<String>, name: String, options: &Options)
         element.assign("class", "vert");
         let txt  = Text::new(&index.name);
         element.append(txt);
-        element.assign("transform","rotate(90 ".to_owned()+&(index.x - 5.0).to_string()+","+&(index.y + 10.0).to_string()+")");
+        element.assign("transform","rotate(90 ".to_owned()+&(index.x - 5.0).to_string()
+        + ","+&(index.y + 10.0).to_string()+")");
         match tree.is_leaf(index.idx) {
             true => g.append(element),
             _   =>  match options.gene_internal {
@@ -87,15 +90,20 @@ pub fn draw_tree (tree: &mut ArenaTree<String>, name: String, options: &Options)
 }
 
 /// Draw a svg pipe species tree and  several gene trees inside it
-pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std::vec::Vec<ArenaTree<String>>, name: String, options: &Options) {
+pub fn draw_sptree_gntrees (
+    sp_tree: &mut ArenaTree<String>,
+    gene_trees:&mut std::vec::Vec<ArenaTree<String>>,
+    name: String, options: &Options
+    ) {
     let largest_x = sp_tree.get_largest_x() * 1.0 + 0.0 ;
     let largest_y = sp_tree.get_largest_y() * 1.0 + 0.0 ;
     let smallest_x = sp_tree.get_smallest_x() * 1.0 + 0.0 ;
     let smallest_y = sp_tree.get_smallest_y() * 1.0 + 0.0 ;
     let width_svg = largest_x - smallest_x+ 1.0 * BLOCK;
     let width_svg = width_svg * 1.0;
-    let height_svg = largest_y - smallest_y + 2.0 * BLOCK; // Crade ; ajout car les fuielles de l'espece sont
-                                                           // allongées lors de la creation du le path
+    let height_svg = largest_y - smallest_y + 2.0 * BLOCK;  // Ajout car les fuielles de l'espece
+                                                            // sont allongées lors de la creation
+                                                            // du  path . A ameliorer
     let height_svg = height_svg * 1.0;
     let x_viewbox = smallest_x - 0.0 ;
     let y_viewbox = smallest_y - 0.0;
@@ -115,10 +123,14 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
         match index.parent {
             Some(p) => {
                 let n = &sp_tree.arena[p];
-                let chemin = get_chemin_sp(index.x, index.y, index.width/2.0, index.height/2.0, n.x, n.y,n.width/2.0,n.height/2.0);
+                let chemin = get_chemin_sp(index.x, index.y,
+                                           index.width/2.0, index.height/2.0,
+                                           n.x, n.y,
+                                           n.width/2.0, n.height/2.0);
                 g.append(chemin);
                 if sp_tree.is_leaf(index.idx) {
-                    let chemin = close_chemin_sp(index.x, index.y, index.width/2.0, index.height/2.0);
+                    let chemin = close_chemin_sp(index.x, index.y,
+                                                 index.width/2.0, index.height/2.0);
                     g.append(chemin);
                 }
             },
@@ -133,7 +145,8 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
                 element.assign("class", "jaune");
                 let txt  = Text::new(&index.name);
                 element.append(txt);
-                element.assign("transform","rotate(90 ".to_owned()+&index.x.to_string()+","+&index.y.to_string()+")");
+                element.assign("transform","rotate(90 ".to_owned() + &index.x.to_string()
+                + "," + &index.y.to_string() + ")" );
                 g.append(element);
             },
             false => {
@@ -144,7 +157,8 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
                         element.assign("class", "jaune");
                         let txt  = Text::new(&index.name);
                         element.append(txt);
-                        element.assign("transform","rotate(90 ".to_owned()+&index.x.to_string()+","+&index.y.to_string()+")");
+                        element.assign("transform","rotate(90 ".to_owned()+&index.x.to_string()
+                        + "," + &index.y.to_string() + ")" );
                         g.append(element);
                     },
                     false => {},
@@ -170,7 +184,8 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
             .luminosity(Luminosity::Bright) // Optional
             .alpha(1.0) // Optional
             .to_rgb_string(); //
-        let style = Style::new(".gene_".to_owned()+&idx_rcgen.to_string()+" { font:  10px serif; fill:"+&gene_color.to_string()+"; }");
+        let style = Style::new(".gene_".to_owned()+&idx_rcgen.to_string()
+            + " { font:  10px serif; fill:" + &gene_color.to_string() + "; }" );
         document.append(style);
         for  index in &gene_trees[idx_rcgen].arena {
             // Dessine le chemin du noeud a son pere
@@ -182,9 +197,17 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
                         true => {
                             // Verifie que le parent est bien un branchingout
                             match n.e {
-                                Event::BranchingOut => get_chemin_transfer(index.x,index.y,n.x,n.y,gene_color.to_string()),
-                                Event::BifurcationOut => get_chemin_transfer(index.x,index.y,n.x,n.y,gene_color.to_string()),
-                                _ => panic!("Wrong recPhyloXML feature. The father node should be BranchingOut or BifurcationOut, but I found a {:?}\n{:?}",n.e,n),
+                                Event::BranchingOut => get_chemin_transfer(index.x,index.y,
+                                                                           n.x,n.y,
+                                                                           gene_color.to_string()
+                                                                           ),
+                                Event::BifurcationOut => get_chemin_transfer(index.x,index.y,
+                                                                            n.x,n.y,
+                                                                            gene_color.to_string()
+                                                                            ),
+                                _ => panic!("Wrong recPhyloXML feature.
+                                The father node should be BranchingOut or
+                                BifurcationOut, but I found a {:?}\n{:?}",n.e,n),
                             }
                         },
                         false => get_chemin_carre(index.x,index.y,n.x,n.y,gene_color.to_string()),
@@ -196,10 +219,11 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
                     let mut element = Element::new("text");
                     element.assign("x", index.x+10.0);
                     element.assign("y", index.y+0.0);
-                    element.assign("class", "gene_".to_owned()+&idx_rcgen.to_string());
+                    element.assign("class", "gene_".to_owned() + &idx_rcgen.to_string());
                     let txt  = Text::new(&idx_rcgen.to_string());
                     element.append(txt);
-                    element.assign("transform","rotate(90 ".to_owned()+&index.x.to_string()+","+&index.y.to_string()+")");
+                    element.assign("transform","rotate(90 ".to_owned() + &index.x.to_string()
+                    + "," + &index.y.to_string() + ")" );
                     g.append(element);
                  },
              };
@@ -207,35 +231,29 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
              let  event = &index.e;
              match event {
                  Event::Leaf        =>  g.append(get_carre(index.x,index.y,1.0,"red".to_string())),
-                 Event::Duplication =>  g.append(get_carre(index.x,index.y,4.0,gene_color.to_string())),
+                 Event::Duplication =>  g.append(get_carre(index.x,index.y,4.0,
+                                                 gene_color.to_string())
+                                                 ),
                  Event::Loss => {
                      let mut cross = get_cross(index.x,index.y,2.0,gene_color.to_string());
-                     cross.assign("transform","rotate(45 ".to_owned()+&index.x.to_string()+" "+&index.y.to_string()+")");
+                     cross.assign("transform","rotate(45 ".to_owned() + &index.x.to_string()
+                     + " " + &index.y.to_string() + ")" );
                      g.append(cross);
                  },
                 // Normalement il ny' a pas d event transferBack : comme il est toujour associé
                 // a un autre event,c'est ce dernier qui est stocké dans le champs "e"
                 // Par contre le champs "is_a_transfert" indique si le noeud prvient d'un transfer
 
-                //  Event::TransferBack => {
-                //      let _parent =  match index.parent {
-                //          Some(p) => {
-                //             let n = &gene_trees[idx_rcgen].arena[p];
-                //             let diamond = get_carre(n.x,n.y,4.0,"green".to_string());
-                //             g.assign("transform","rotate(45 ".to_owned()+&n.x.to_string()+" "+&n.y.to_string()+")");
-                //             g.append(diamond);
-                //         },
-                //         None =>{ panic!("The TransferBack Node as no father {:?}",index) },
-                //     };
-                // },
                 Event::BranchingOut  =>  {
                     let mut diamond = get_carre(index.x,index.y,4.0,gene_color.to_string());
-                    diamond.assign("transform","rotate(45 ".to_owned()+&index.x.to_string()+" "+&index.y.to_string()+")");
+                    diamond.assign("transform","rotate(45 ".to_owned() + &index.x.to_string()
+                    + " " + &index.y.to_string() + ")" );
                     g.append(diamond);
                     },
                 // Est ce que BifurcationOut existe encore ???
-                Event::BifurcationOut  =>  g.append(get_carre(index.x,index.y,5.0,"yellow".to_string())),
-                _                  =>  g.append(get_circle(index.x,index.y,3.0,gene_color.to_string())),
+                Event::BifurcationOut  =>  g.append(get_carre(index.x,index.y,5.0,
+                                                    "yellow".to_string())),
+                _ =>  g.append(get_circle(index.x,index.y,3.0,gene_color.to_string())),
             };
             // Affiche le texte associe au noeud
             match event {
@@ -246,7 +264,8 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
                     element.assign("class", "gene_".to_owned()+&idx_rcgen.to_string());
                     let txt  = Text::new(&index.name);
                     element.append(txt);
-                    element.assign("transform","rotate(90 ".to_owned()+&index.x.to_string()+","+&index.y.to_string()+")");
+                    element.assign("transform","rotate(90 ".to_owned() + &index.x.to_string()
+                    + "," + &index.y.to_string() + ")" );
                     g.append(element);
                     },
                 _ => {
@@ -255,10 +274,11 @@ pub fn draw_sptree_gntrees (sp_tree: &mut ArenaTree<String>, gene_trees:&mut std
                             let mut element = Element::new("text");
                             element.assign("x", index.x+10.0);
                             element.assign("y", index.y+0.0);
-                            element.assign("class", "gene_".to_owned()+&idx_rcgen.to_string());
+                            element.assign("class", "gene_".to_owned() + &idx_rcgen.to_string());
                             let txt  = Text::new(&index.name);
                             element.append(txt);
-                            element.assign("transform","rotate(90 ".to_owned()+&index.x.to_string()+","+&index.y.to_string()+")");
+                            element.assign("transform","rotate(90 ".to_owned()
+                            + &index.x.to_string() + "," + &index.y.to_string()+")");
                             g.append(element);
                         },
                         false => {},
@@ -387,12 +407,21 @@ pub fn get_chemin_transfer (x1: f32, y1:f32,x2: f32, y2:f32, c:String) -> Path {
     let controle_x = (x1 + x2) / 2.0 ;
     let controle_y = (y1 + y2) / 2.0 - bez_y ;
     // ligne horizontale
-    let data = "M".to_owned()+&x1.to_string()+" "+&initial_y1.to_string()+" L "+ &x1.to_string()+" "+ &y1.to_string();
+    let data = "M".to_owned() + &x1.to_string() +" " + &initial_y1.to_string()
+             +" L "+ &x1.to_string() + " " + &y1.to_string();
     // fleche
-    let data = data.to_owned()+ "M"+&x1.to_string() + " " + &(initial_y1- PIPEBLOCK / 4.0).to_string() + "L "+ &(x1 - PIPEBLOCK / 4.0 ).to_string() + " " + &(initial_y1 - PIPEBLOCK / 2.0 ).to_string();
-    let data = data.to_owned()+ "M"+&x1.to_string() + " " + &(initial_y1- PIPEBLOCK / 4.0).to_string() + "L "+ &(x1 + PIPEBLOCK / 4.0 ).to_string() + " " + &(initial_y1 - PIPEBLOCK / 2.0 ).to_string();
+    let data = data.to_owned()+ "M" + &x1.to_string() + " "
+              + &(initial_y1- PIPEBLOCK / 4.0).to_string() + "L "
+              + &(x1 - PIPEBLOCK / 4.0 ).to_string() + " "
+              + &(initial_y1 - PIPEBLOCK / 2.0 ).to_string();
+    let data = data.to_owned()+ "M"+&x1.to_string() + " "
+               + &(initial_y1- PIPEBLOCK / 4.0).to_string() + "L "
+               + &(x1 + PIPEBLOCK / 4.0 ).to_string() + " "
+               + &(initial_y1 - PIPEBLOCK / 2.0 ).to_string();
     // bezier
-    let data = data.to_owned() + "M"+&x1.to_string()+" "+&y1.to_string()+" Q "+&controle_x.to_string()+" "+&controle_y.to_string()+" "+&x2.to_string()+" "+&y2.to_string();
+    let data = data.to_owned() + "M" + &x1.to_string() + " " + &y1.to_string()
+               + " Q " + &controle_x.to_string() + " " + &controle_y.to_string()
+               + " " + &x2.to_string() + " " + &y2.to_string();
     let path = Path::new()
     .set("fill", "none")
     .set("stroke", c)
@@ -417,7 +446,8 @@ pub fn get_chemin_simple (x1: f32, y1:f32,x2: f32, y2:f32) -> Path {
 }
 
 /// Draw a square pipe path between x1,y1 ad x2,y2
-pub fn get_chemin_sp (x1: f32, y1:f32, width1:f32, height1:f32, x2: f32, y2:f32, width2:f32, height2:f32 ) -> Path {
+pub fn get_chemin_sp (x1: f32, y1:f32, width1:f32, height1:f32, x2: f32, y2:f32,
+                      width2:f32, height2:f32 ) -> Path {
     if x1 < x2 {
         let data = Data::new()
         .move_to((x1 - width1, y1 - height1))
