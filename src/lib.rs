a6c2c89543673d41333fb015dd0d985b94de70af
Simon Penel <simon.penel@univ-lyon1.fr>
2021-04-02 09:34:14 +0200

    Correction of tests

--- src/lib.rs
+++ src/lib.rs
@@ -57,74 +57,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn  check_val() {
-        let mut tree: ArenaTree<String> = ArenaTree::default();
-        let test = String::from("Test");
-        let copy = test.clone();
-        let index = tree.new_node(test);
-        let mut node = &mut tree.arena[index];
-        let val = node.get_val();
-        assert_eq!(*val,copy);
-    }
-
-    #[test]
     fn  check_event() {
         let mut tree: ArenaTree<String> = ArenaTree::default();
         let test = String::from("Test");
-        let copy = test.clone();
         let index = tree.new_node(test);
-        let mut node = &mut tree.arena[index];
-        node.set_event(Event::Undef);
-        let event = node.get_event();
-        assert_eq!(*event,Event::Undef);
-    }
-    #[test]
-    fn  check_coords_noref() {
-        let mut tree: ArenaTree<String> = ArenaTree::default();
-        let test = String::from("Test");
-        let copy = test.clone();
-        let index = tree.new_node(test);
-        let mut node = &mut tree.arena[index];
-        node.set_x_noref(10.0);
-        node.set_y_noref(20.0);
-        let (x,y) = node.get_coords();
-        assert_eq!((*x,*y),(10.0,20.0));
-    }
-    #[test]
-    fn  check_coords() {
-        let mut tree: ArenaTree<String> = ArenaTree::default();
-        let test = String::from("Test");
-        let copy = test.clone();
-        let index = tree.new_node(test);
-        let mut node = &mut tree.arena[index];
-        node.set_x(&10.0);
-        node.set_y(&20.0);
-        let coords = node.get_coords();
-
-        assert_eq!(coords,(&10.0,&20.0));
+        let node = &mut tree.arena[index];
+        node.set_event(Event::Duplication);
+        assert_eq!(node.e,Event::Duplication);
     }
     #[test]
     fn  check_x_noref() {
         let mut tree: ArenaTree<String> = ArenaTree::default();
         let test = String::from("Test");
-        let copy = test.clone();
         let index = tree.new_node(test);
-        let mut node = &mut tree.arena[index];
+        let node = &mut tree.arena[index];
         node.set_x_noref(10.0);
-        let x = node.get_x();
-        assert_eq!(*x, 10.0);
+        let x = node.x;
+        assert_eq!(x, 10.0);
     }
-    #[test]
-    fn  check_x() {
-        let mut tree: ArenaTree<String> = ArenaTree::default();
-        let test = String::from("Test");
-        let copy = test.clone();
-        let index = tree.new_node(test);
-        let mut node = &mut tree.arena[index];
-        node.set_x(&10.0);
-        let x = node.get_x();
-        assert_eq!(*x, 10.0);
-    }
+
 
 
 
