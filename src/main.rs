f4d3fefee422f06ddc051ab016a73173f66dfe35
Simon Penel <simon.penel@univ-lyon1.fr>
2021-04-07 11:49:25 +0200

    Code cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 /// name = "rectree2svg"
-/// version = "0.1.4"
+/// version = "0.5.0"
 /// authors = ["Simon Penel <simon.penel@univ-lyon1.fr>"]
-/// release = "30/03/2021"
+/// release = "07/04/2021"
 /// license = "CECILL-2.1"
 ///
 /// Usage:
